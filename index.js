7e9cba1cd5c62ec6b1fff2253d108551c35f0b67
Orza Adrian <adi.orza@yahoo.com>
2023-06-14 21:15:51 +0300

    add showhome and showskills functions

---
+++ index.js
@@ -0,0 +1,12 @@
+function showHome(){
+    var page = document.getElementById('home');
+    page.style.display = "block";
+}
+
+
+function showSkills(){
+    var page = document.getElementById('skills');
+    page.style.display = "block";
+}
+
+showHome();
